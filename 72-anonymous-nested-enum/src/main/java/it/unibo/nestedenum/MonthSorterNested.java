6c6cf01b80e985a4e5808bc56a6de42ee1e8e68b
GioeleBucci <gioele.bucci@studio.unibo.it>
2023-11-06 20:24:05 +0100

    everything done

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,6 +1,9 @@
 package it.unibo.nestedenum;
 
+import java.sql.Array;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 
@@ -9,13 +12,70 @@ import java.util.Objects;
  */
 public final class MonthSorterNested implements MonthSorter {
 
+  public enum Month {
+    JANUARY(31),
+    FEBRUARY(28),
+    MARCH(31),
+    APRIL(30),
+    MAY(31),
+    JUNE(30),
+    JULY(31),
+    AUGUST(31),
+    SEPTEMBER(30),
+    OCTOBER(31),
+    NOVEMBER(30),
+    DECEMBER(31);
+
+    public final int days;
+
+    private Month(final int days) {
+      this.days = days;
+    }
+
+    public static Month fromString(String str) {
+      str = str.toUpperCase();
+      Month match = null;
+      for (Month month : Month.values()) {
+        for (int i = 0; i < str.length(); i++) {
+          if (month.toString().charAt(i) != str.charAt(i)) {
+            break;
+          }
+          // found a match
+          if (i == (str.length() - 1)) {
+            if (match != null) {
+              throw new IllegalArgumentException("string " + str + " is ambiguous");
+            }
+            match = month;
+          }
+        }
+      }
+      if (match == null) {
+        throw new IllegalArgumentException("string " + str + " does not match with any month");
+      }
+      return match;
+    }
+  }
+
   @Override
   public Comparator<String> sortByDays() {
-    return null;
+    return new Comparator<String>() {
+      @Override
+      public int compare(String m1, String m2) {
+        return Month.fromString(m1).days - Month.fromString(m2).days;
+      }
+    };
   }
 
   @Override
   public Comparator<String> sortByOrder() {
-    return null;
+    return new Comparator<String>() {
+      @Override
+      public int compare(String m1, String m2) {
+        //var months = Arrays.asList(Month.values());
+        var months = List.of(Month.values());
+        System.out.println(months);
+        return months.indexOf(Month.fromString(m1)) - months.indexOf(Month.fromString(m2));
+      }
+    };
   }
 }
