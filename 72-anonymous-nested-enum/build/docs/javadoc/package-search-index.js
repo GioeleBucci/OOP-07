0787aafc4de1e184599a271ba5dfe1c1a044c638
Gioele Bucci <gioele.bucci@studio.unibo.it>
2023-11-08 10:40:20 +0100

    Delete 72-anonymous-nested-enum/build directory

--- 72-anonymous-nested-enum/build/docs/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"it.unibo.functional"},{"l":"it.unibo.functional.api"},{"l":"it.unibo.nestedenum"}];updateSearchResults();
